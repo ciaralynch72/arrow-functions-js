7b5a120d4d91b316a2ba6e6a937c5371be0f9b1f
ciaralynch72 <Ciaralynch72@gmail.com>
2022-10-17 23:47:05 +0000

    Practice functions

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,11 +4,22 @@
  */
 
 // Vanilla JavaScript Function
-
+    function addTwoNumbers(a, b) {
+    return a + b;
+    }
+    
+    let sum = addTwoNumbers(4,6)
+    console.log(sum)
 // Arrow Function With Parameters
-
+const addTwoNumbers1 = (a,b) => {
+    return a + b;
+}
+let sum1 = addTwoNumbers1 (4,7)
+console.log(sum1)
 // Single Line Arrow Function With Parameters
-
+const addTwoNumbers2 = (a,b) => a + b;
+let sum2 = addTwoNumbers2 (2,7)
+console.log(sum2)
 // Implicit Returns
 
 // Returning Multiple Lines
